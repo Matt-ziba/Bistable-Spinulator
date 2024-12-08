e6dda39a37aed128367acf9dcd42a228803fd95b
zibam.08 <zibam.08@spst.eu>
2024-12-08 22:18:22 +0100

    Added scoreboard, press detection and collision detection for enemy circles

--- src/main/java/com/bistable_spinulator/bistable_spinulator/main.java
+++ src/main/java/com/bistable_spinulator/bistable_spinulator/main.java
@@ -2,52 +2,86 @@ package com.bistable_spinulator.bistable_spinulator;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Timer;
+import javax.security.auth.login.AppConfigurationEntry;
+import java.util.*;
 
 
 public class main extends Application {
 
-    public static Date now;
-    private final double angularSpeed = 10;
+    private final double angularSpeed = 20;
     private final double width = Screen.getPrimary().getVisualBounds().getWidth(); double height = Screen.getPrimary().getVisualBounds().getHeight();
     private final double[] center = {width/2, height/2};
     private final double offsetY = 120;
     private final double offsetX = 450;
     private final double[] revolveAxis = {center[0] - offsetX, center[1]};
-    private final long spawnPeriod = 1000;
+    private final long spawnPeriod = 800;
     private double lastSpawnTime = 1000;
     private double startTime = 0;
     private double angle = 0;
+    private int score = 0;
 
     private boolean rotate = false;
 
-    Circle PC1 = new Circle(40.0f, Color.rgb(255,0,0));
-    Circle PC2 = new Circle(40.0f, Color.rgb(0,0,255));
+    Circle PC1 = new Circle(40.0f, Color.rgb(224,108,117));
+    Circle PC2 = new Circle(40.0f, Color.rgb(80,181,192));
+    Text ScoreBoard = new Text(String.valueOf(score));
 
     private List<Circle> movingCircle = new ArrayList<>();
+    private List<Circle> toBeRemoved = new ArrayList<>();
+
+    public Group mainGroup = new Group(PC1,PC2, ScoreBoard);
 
     @Override
     public void start(Stage stage) {
         Timer period = new Timer();
+        ScoreBoard.setTranslateX(center[0]); ScoreBoard.setTranslateY(center[1] - 300);
+        ScoreBoard.setFill(Color.rgb(171,178,191));
+        ScoreBoard.setScaleX(5); ScoreBoard.setScaleY(5);
         PC1.setTranslateX(revolveAxis[0]); PC1.setTranslateY(revolveAxis[1] + offsetY);
         PC2.setTranslateX(revolveAxis[0]); PC2.setTranslateY(revolveAxis[1] - offsetY);
 
-        Group mainGroup = new Group(PC1,PC2);
         Scene scene = new Scene(mainGroup, width, height);
 
         scene.setOnKeyPressed(keyEvent -> {
             if(keyEvent.getCode().toString().equals("SPACE")) {
                 rotate = true;
+            }
+            if(keyEvent.getCode().toString().equals("S") || keyEvent.getCode().toString().equals("DOWN")) {
+                for(Circle enemy : movingCircle) {
+                    if((enemy.getTranslateX() > 278 && enemy.getTranslateX() < 358) && enemy.getTranslateY() == revolveAxis[1] + offsetY) {
+                        if(angle == 0 && enemy.getFill().equals(Color.rgb(224,108,117))) {
+                            killEnemy(enemy);
+                            incrementScore();
+                        } else if (angle == 200 && enemy.getFill().equals(Color.rgb(80, 181, 192))) {
+                            killEnemy(enemy);
+                            incrementScore();
+                        }
+
+                    }
+                }
+            }
+            if(keyEvent.getCode().toString().equals("W") || keyEvent.getCode().toString().equals("UP")) {
+                for(Circle enemy : movingCircle) {
+                    if((enemy.getTranslateX() > 278 && enemy.getTranslateX() < 358) && enemy.getTranslateY() == revolveAxis[1] - offsetY) {
+                        if(angle == 200 && enemy.getFill().equals(Color.rgb(224,108,117))) {
+                            killEnemy(enemy);
+                            incrementScore();
+                        } else if (angle == 0 && enemy.getFill().equals(Color.rgb(80, 181, 192))) {
+                            killEnemy(enemy);
+                            incrementScore();
+                        }
+
+                    }
+                }
             }
         });
 
@@ -62,26 +96,26 @@ public class main extends Application {
                     double y1 = revolveAxis[1] + offsetY * Math.cos(rad);
                     double x2 = revolveAxis[0] + offsetY * Math.sin(rad + Math.PI);
                     double y2 = revolveAxis[1] + offsetY * Math.cos(rad + Math.PI);
-                    if(angle >= 190) {
-                        PC2.setTranslateX(x1); PC1.setTranslateY(y1);
-                        PC1.setTranslateX(x2); PC2.setTranslateY(y2);
-                    }
+
                     PC1.setTranslateX(x1); PC1.setTranslateY(y1);
                     PC2.setTranslateX(x2); PC2.setTranslateY(y2);
 
                     angle += angularSpeed;
 
-                    if(angle == 190) {
-                        angle = 190;
+                    if(angle == 200) {
+                        angle = 200;
                         rotate = false;
                     }
-                    if(angle % 370 == 0) {
+                    if(angle == 380) {
                         angle = 0;
                         rotate = false;
                     }
                 }
                 for(Circle enemy : movingCircle) {
-                    double newX = enemy.getTranslateX() - 15;
+                    double newX = enemy.getTranslateX() - 10;
+                    if(enemy.getTranslateX() < 0) {
+                        toBeRemoved.add(enemy);
+                    }
                     enemy.setTranslateX(newX);
                 }
 
@@ -90,8 +124,22 @@ public class main extends Application {
         timer.start();
         lastSpawnTime = System.nanoTime();
 
+        Timer timerTask = new Timer();
+        timerTask.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(() -> {
+                    Circle newCircle = circleSpawn();
+                    mainGroup.getChildren().add(newCircle);
+                    movingCircle.add(newCircle);
+                    for(Circle enemy : toBeRemoved) {
+                        removeEnemy(enemy);
+                    }
+                });
+            }
+        }, 0, spawnPeriod);
 
-
+        scene.setFill(Color.rgb(40,44,52));
         stage.setTitle("Bistable Spinulator");
         stage.setScene(scene);
         stage.show();
@@ -102,25 +150,39 @@ public class main extends Application {
         launch();
     }
 
+
     public Circle circleSpawn() {
 
         Color color;
         double pos;
 
         if(Math.random() > 0.5) {
-            color = Color.RED;
+            color = Color.rgb(224,108,117);
         } else {
-            color = Color.BLUE;
+            color = Color.rgb(80,181,192);
         }
+
+        Circle enemy = new Circle(40.0f, color);
 
         if(Math.random() > 0.5) {
-            pos = center[1] + offsetY;
-        } else {
-            pos = center[1] - offsetY;
+            enemy.setTranslateY(revolveAxis[1] + offsetY); enemy.setTranslateX(width);
         }
-        System.out.println("new circle");
-        return new Circle(width, pos, 40, color);
-
+        else {
+            enemy.setTranslateY(revolveAxis[1] - offsetY); enemy.setTranslateX(width);
+        }
+        return  enemy;
+    }
+    public void removeEnemy(Circle enemy) {
+        movingCircle.remove(enemy);
+        mainGroup.getChildren().remove(enemy);
+    }
+    public void killEnemy(Circle enemy) {
+        movingCircle.remove(enemy);
+        mainGroup.getChildren().remove(enemy);
+    }
+    public void incrementScore() {
+        score++;
+        ScoreBoard.setText(String.valueOf(score));
     }
 
 }
